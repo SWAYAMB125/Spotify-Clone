f45b98e3cdf8bc7d236dc06e978e76658f55d0a6
SWAYAMB125 <swayam.bole@gmail.com>
2024-11-10 02:55:38 +0530

    frontend setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+
+  return (
+    <>
+    <h1 className="text-red-500 text-5xl">this is a test</h1>
+    <Button>test button</Button>
+    </>
+  )
+}
+
+export default App
